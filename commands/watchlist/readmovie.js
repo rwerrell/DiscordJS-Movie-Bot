3f726c7dd000b3e2544fe990c3717a228cf213a0
rwerrell <rhyswerrell@gmail.com>
2025-03-17 20:06:58 +0000

    Fixed read movie watchlist

--- commands/watchlist/readmovie.js
+++ commands/watchlist/readmovie.js
@@ -1,20 +1,29 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const jsonfile = require('jsonfile');
+const { Pagination } = require('pagination.djs');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('readmovies')
+        .setName('watchlist')
         .setDescription('Lists all the movies in the watchlist!'),
     async execute(interaction) {
         jsonfile.readFile('movies.json', function (err, movieList) {
             if (err) {
                 console.error(err);
+                interaction.reply({ content: 'An error occurred while reading the watchlist.', ephemeral: true });
                 return;
             }
 
             // Ensure movieList is an array
             if (!Array.isArray(movieList)) {
                 console.error('JSON content is not an array');
+                interaction.reply({ content: 'The watchlist is not in the correct format.', ephemeral: true });
+                return;
+            }
+
+            // Check if the movie list is empty
+            if (movieList.length === 0) {
+                interaction.reply({ content: 'The watchlist is empty.', ephemeral: true });
                 return;
             }
 
@@ -24,17 +33,21 @@ module.exports = {
                 return {
                     name: `**${movie.Movie}**`, // Make the movie title bold
                     value: `Added by: ${movie['Added By']}\nDate Added: ${movie['Date Added']}\n[Search](${movieURL})`,
-                    inline: true
+                    inline: false
                 };
             });
 
-            var MovieListEmbed = new EmbedBuilder()
-                .setColor('DarkGreen')
-                .setTitle(`Kitten Cave Movie Watchlist`)
+            // Create a Pagination instance
+            const MovieList = new Pagination(interaction)
+                .setLimit(8)
+                .setTitle('Kitten Cave Movie Watchlist')
+                .setColor('#00ff00')
                 .addFields(fields)
-                .setAuthor({name: `${interaction.user.username}`, iconURL: `${interaction.user.avatarURL({extension: 'png'})}`});
+                .setAuthor({ name: `${interaction.user.username}`, iconURL: `${interaction.user.avatarURL({ extension: 'png' })}` })
+                .paginateFields(true);
 
-            interaction.reply({ embeds: [MovieListEmbed] });
+            // Render the pagination (this sends the paginated embed to the user)
+            MovieList.render();
         });
-    },  
+    },
 };
